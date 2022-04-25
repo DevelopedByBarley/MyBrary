b34b868c3c4a4b0a83cfa42c47c84b1ebf0be8bf
Árpád <developedbybarley@gmail.com>
2022-04-25 09:49:29 +0200

    Initial Author routes

--- server.js
+++ server.js
@@ -4,16 +4,20 @@ if(process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const app = express();
-const expressLayouts = require('express-ejs-layouts');
-const indexRouter = require('./routes/index')
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-app.set('view engine', 'ejs');
+const expressLayouts = require('express-ejs-layouts');
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
+app.set('view engine', 'ejs');
 app.set('views', __dirname + "/views");
 app.set('layout', 'layouts/layout');
+
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 mongoose.connect( process.env.DATABASE_URL, {useNewUrlParser: true})
 const db = mongoose.connection;
@@ -21,5 +25,6 @@ db.on('error', error => console.log(error))
 db.once('open', () => console.log("Connected to Mongoose") )
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000)
